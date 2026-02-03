007a81f0325c21f6e41cb4fd402354fac07e5714
piyushzade00 <piyushzade00@gmail.com>
2026-02-03 15:07:03 +0530

    Initial commit

---
+++ subscription-api-backend/src/main/java/com/piyush/api/subscription_api_backend/SubscriptionApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.api.subscription_api_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApiBackendApplication.class, args);
+	}
+
+}
---
+++ subscription-api-backend/src/test/java/com/piyush/api/subscription_api_backend/SubscriptionApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.api.subscription_api_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
